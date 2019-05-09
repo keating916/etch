581eb9fa14637ec5540a15530019814e26dc1418
Patrick Keating <keating916@gmail.com>
2019-05-08 20:33:08 -0700

    add c keypress for change color/ finish

--- etch.js
+++ etch.js
@@ -1,17 +1,24 @@
-let container = document.getElementById("container"); //get container element for grid
- // create div element for grid
- let v = 16;
 let header = document.getElementById('header');
 header.style= "display: flex; width: 400px;;"
-container.style.cssText = `display: grid; grid-template-columns: repeat(${v}, 1fr); grid-template-rows: repeat(${v}, 1fr); height: 400px; width: 400px; border: 1px solid black; padding: auto;`;
 let button = document.getElementById('clear')
 button.style.height = "auto"; 
-generateDivs(v)
+generateDivs(16);
+let newColor = colorGenerator();
+
+document.addEventListener("keyup", e => {
+    console.log(e.keyCode)
+    if(e.keyCode == 67) {
+        newColor = colorGenerator();
+    }
+});
+
 function colorGenerator() {
     let color ='#'+Math.floor(Math.random()*16777215).toString(16);
     return color
 }
-function generateDivs(x) {
+function generateDivs(x) {  // create div element for grid
+    let container = document.getElementById("container"); //get container element for grid
+    container.style.cssText = `display: grid; grid-template-columns: repeat(${x}, 1fr); grid-template-rows: repeat(${x}, 1fr); height: 400px; width: 400px; border: 1px solid black; padding: auto;`;
     for(let i=0; i<x**2; i++) {
         let grid = document.createElement("div");
         grid.style = `background-color: white`
@@ -19,24 +26,32 @@ function generateDivs(x) {
         grid.id = i;
         container.appendChild(grid)
     }
-}
-function changeColor() {
-    let color = colorGenerator()
-    return color;
-}
-let totalGrid = document.getElementsByClassName("gridItems");
-for(let i = 0; i < totalGrid.length; i++) {
-    let item = document.getElementById(`${i}`);
-    item.addEventListener("mouseover", function() {
-        item.style = `background-color: ${changeColor()}`
-    })
+    changeColor();
 };
-button.addEventListener('click', e => {
-    let input = prompt("How many squares would you like across?")
-    v=input;
+function changeColor() {
+    let totalGrid = document.getElementsByClassName("gridItems")
     for(let i = 0; i < totalGrid.length; i++) {
-        let item = document.getElementById(`${i}`)
-        document.removeChild(container);
+        let item = document.getElementById(`${i}`);
+        item.addEventListener("mouseover", function() {
+            item.style = `background-color: ${newColor}`
+        })
+    };
+};
+
+
+function removeGrid() {
+    let totalGrid = document.getElementsByClassName("gridItems").length
+    for(let i = 0; i< totalGrid; i++) {
+        el = document.getElementById(`${i}`);
+        el.parentNode.removeChild(el);
+        console.log(`removed ${i}`);
     }
-    generateDivs(v);
+//removes old grid
+}
+
+button.addEventListener('click', e => {
+    let input = prompt("How many squares would you like across?");
+    input = Number.parseInt(input);
+    removeGrid();
+    generateDivs(input);
 })
